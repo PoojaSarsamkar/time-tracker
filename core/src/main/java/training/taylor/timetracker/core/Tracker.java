7419ed0cd8a00f73b4f7b070ce2e93fefb588cb5
PoojaSarsamkar <153292589+PoojaSarsamkar@users.noreply.github.com>
2024-02-10 11:37:00 +0530

    Update Tracker.java
    Edited

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -1,7 +1,7 @@
 package training.taylor.timetracker.core;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
@@ -9,7 +9,7 @@ import java.util.List;
 @Component
 public class Tracker {
     @Autowired
-   // private List<TimeEntry> entries;
+    private List<TimeEntry> entries;
 
     public void add(TimeEntry entry) {
         entries.add(entry);
